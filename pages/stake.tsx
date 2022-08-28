6003e884b44d8a32862df82d470324ac2dd75e47
myreceipt <ma2k@straight-line.org>
2022-08-29 03:52:47 +0800

    Font Size

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -110,7 +110,7 @@ const Stake: NextPage = () => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.h1}>- The ROTY BROI Staking -</h1>
+      <h1 className={styles.h1}>The ROTY BROI Staking</h1>
 
       <hr className={`${styles.divider} ${styles.spacerTop}`} />
 
